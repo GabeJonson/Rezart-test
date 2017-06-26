b90bbecf34bb4ae7dfe6df4b1ddb76ba1c475eaa
Gabe Jonson <sky3107@gmail.com>
2017-06-26 19:41:48 +0300

    Header done

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
-
+  Rezart.init();
 });
+
+var Rezart = (function() {
+  return {
+    init: function() {
+      this.owl();
+      this.sandwitch();
+    },
+
+    owl: function() {
+    },
+
+    sandwitch: function() {
+      $('.sandwitch').click(function() {
+        $(this).toggleClass('active');
+
+        $('.nav').slideToggle('fast');
+      });
+    },
+
+    smooth: function() {
+      $('a[href=#]').bind('click', function(e){
+      	e.preventDefault();
+
+      	$('html, body').stop().animate({
+      		scrollTop: $( $(this).attr('href')).offset().top
+      	}, 500);
+      });
+    }
+  }
+})();
